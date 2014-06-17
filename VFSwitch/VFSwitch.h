3df90d125c05e328a664d0fcf2b8bb614ff35c57
xiaoym <xiaoym@me.com>
2014-06-17 17:21:51 +0800

    Initial Commit

---
+++ VFSwitch/VFSwitch.h
@@ -0,0 +1,19 @@
+//
+//  VFSwitch.h
+//  VFSwitch
+//
+//  Created by 肖宇明 on 14/6/17.
+//  Copyright (c) 2014年 VoidFaceless. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for VFSwitch.
+FOUNDATION_EXPORT double VFSwitchVersionNumber;
+
+//! Project version string for VFSwitch.
+FOUNDATION_EXPORT const unsigned char VFSwitchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VFSwitch/PublicHeader.h>
+
+
---
+++ VFSwitchTests/VFSwitchTests.swift
@@ -0,0 +1,35 @@
+//
+//  VFSwitchTests.swift
+//  VFSwitchTests
+//
+//  Created by 肖宇明 on 14/6/17.
+//  Copyright (c) 2014年 VoidFaceless. All rights reserved.
+//
+
+import XCTest
+
+class VFSwitchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
